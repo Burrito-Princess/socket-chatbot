ab38efa6c1d11cb970ae393b2926923e416f445a
Burrito-Princess <willem.lol@outlook.com>
2024-02-16 09:06:03 +0100

    challenge 3 - 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/chat.ejs",
+    "./views/login.ejs"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
